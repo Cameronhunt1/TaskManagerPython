eed889a6bc76bb0c965ff619774d85eb10276a6e
Cameronhunt1 <163554161+Cameronhunt1@users.noreply.github.com>
2024-04-26 15:32:49 +0100

    Add files via upload

---
+++ AItask.py
@@ -0,0 +1,63 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Owner/PycharmProjects/AIpython.py
+
+Task Manager
+1. Adding a Task
+2. Viewing Tasks
+3. Marking a Task as Completed
+4. Deleting a Task
+5. Exit
+Enter your choice, by inputting the number of the action you wish to choose: 1
+Enter task name: dish
+The task was successfully added!
+
+Task Manager
+1. Adding a Task
+2. Viewing Tasks
+3. Marking a Task as Completed
+4. Deleting a Task
+5. Exit
+Enter your choice, by inputting the number of the action you wish to choose: 2
+Tasks:
+Task ID: 1, Task Name: dish, Completed: False
+
+Task Manager
+1. Adding a Task
+2. Viewing Tasks
+3. Marking a Task as Completed
+4. Deleting a Task
+5. Exit
+Enter your choice, by inputting the number of the action you wish to choose: 3
+Enter task ID to mark as completed: 1
+Task marked as completed!
+
+Task Manager
+1. Adding a Task
+2. Viewing Tasks
+3. Marking a Task as Completed
+4. Deleting a Task
+5. Exit
+Enter your choice, by inputting the number of the action you wish to choose: 1
+Enter task name: ee
+The task was successfully added!
+
+Task Manager
+1. Adding a Task
+2. Viewing Tasks
+3. Marking a Task as Completed
+4. Deleting a Task
+5. Exit
+Enter your choice, by inputting the number of the action you wish to choose: 4
+Enter task ID to delete: 1
+The task has been successfully deleted!
+
+Task Manager
+1. Adding a Task
+2. Viewing Tasks
+3. Marking a Task as Completed
+4. Deleting a Task
+5. Exit
+Enter your choice, by inputting the number of the action you wish to choose: 5
+Exiting program
